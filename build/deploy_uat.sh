a1225aba8d80f4201c66d1e73bf87976aa603efe
wengchanghe <wengchanghe@gmail.com>
2018-11-24 16:22:01 -0500

    uat

--- build/deploy_uat.sh
+++ build/deploy_uat.sh
@@ -2,5 +2,5 @@ image_verion=`date +%Y%m%d%H%M`
 echo ${image_version}
 docker container rm -f house-web
 docker build -t house-web .
-docker run -p 48080:8080 -d --name house-web house-web
+docker run -p 38080:8080 -d --name house-web house-web
 
--- build/test_uat.sh
+++ build/test_uat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget http://localhost:48080/myapp-presentation-0.0.1-SNAPSHOT
+wget http://localhost:38080/myapp-presentation-0.0.1-SNAPSHOT
 if [ -e index.html ]; then
    echo "ok" > result.txt
 else 

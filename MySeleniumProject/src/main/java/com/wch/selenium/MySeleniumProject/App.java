ac668a84c99abbf81608dff8d4b449250dbe1996
wengchanghe <wengchanghe@gmail.com>
2018-11-25 00:19:56 -0500

    selenium

---
+++ MySeleniumProject/src/main/java/com/wch/selenium/MySeleniumProject/App.java
@@ -0,0 +1,13 @@
+package com.wch.selenium.MySeleniumProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
